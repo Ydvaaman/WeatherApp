099cc835c9d85e837e72d46b148c421e5364fae2
Aman Yadav <ydvaaman@gmail.com>
2023-11-27 20:52:06 +0530

    updated the code

--- script.js
+++ script.js
@@ -6,35 +6,50 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
-  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  try {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-  if (response.status == 404) {
-    document.querySelector(".error").style.display = "block";
-    document.querySelector(".weather").style.display = "none";
-  } else {
-    var data = await response.json();
+    if (!response.ok) {
+      throw new Error(`City not found (${response.status})`);
+    }
+
+    const data = await response.json();
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML =
-      Math.round(data.main.temp) + "°c";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
 
-    if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = "images/clouds.png";
-    } else if (data.weather[0].main == "Clear") {
-      weatherIcon.src = "images/clear.png";
-    } else if (data.weather[0].main == "Rain") {
-      weatherIcon.src = "images/rain.png";
-    } else if (data.weather[0].main == "Drizzle") {
-      weatherIcon.src = "images/drizzle.png";
-    } else if (data.weather[0].main == "Mist") {
-      weatherIcon.src = "images/mist.png";
+    switch (data.weather[0].main) {
+      case "Clouds":
+        weatherIcon.src = "images/clouds.png";
+        break;
+      case "Clear":
+        weatherIcon.src = "images/clear.png";
+        break;
+      case "Rain":
+        weatherIcon.src = "images/rain.png";
+        break;
+      case "Drizzle":
+        weatherIcon.src = "images/drizzle.png";
+        break;
+      case "Mist":
+        weatherIcon.src = "images/mist.png";
+        break;
+      default:
+        weatherIcon.src = ""; // Set a default image or leave it empty
     }
+
     document.querySelector(".weather").style.display = "block";
     document.querySelector(".error").style.display = "none";
+  } catch (error) {
+    console.error(error.message);
+    document.querySelector(".error").innerHTML = error.message;
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
   }
 }
+
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
 });
